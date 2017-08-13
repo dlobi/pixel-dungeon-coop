bf48ac600e580d54481e1dcc87840529cea9cb15
dlobi <dlobimail@gmail.com>
2017-08-14 00:16:09 +0300

    code reformat

--- core/src/main/java/xyz/dlobi/pixeldungeoncoop/scenes/LobbyScene.java
+++ core/src/main/java/xyz/dlobi/pixeldungeoncoop/scenes/LobbyScene.java
@@ -1,9 +1,13 @@
 package xyz.dlobi.pixeldungeoncoop.scenes;
 
 import xyz.dlobi.noosa.Camera;
+import xyz.dlobi.noosa.Game;
 import xyz.dlobi.pixeldungeoncoop.PixelDungeonCoop;
 import xyz.dlobi.pixeldungeoncoop.ui.Archs;
 import xyz.dlobi.pixeldungeoncoop.ui.ExitButton;
+import xyz.dlobi.pixeldungeoncoop.ui.RedButton;
+import xyz.dlobi.pixeldungeoncoop.ui.TextInput;
+import xyz.dlobi.pixeldungeoncoop.ui.Window;
 import xyz.dlobi.pixeldungeoncoop.windows.WndLobby;
 
 public class LobbyScene extends PixelScene {
@@ -18,18 +22,20 @@ public class LobbyScene extends PixelScene {
         int h = Camera.main.height;
 
         Archs archs = new Archs();
-        archs.setSize( w, h );
-        add( archs );
+        archs.setSize(w, h);
+        add(archs);
 
         WndLobby lobbyWindow = new WndLobby();
-        lobbyWindow.select( 0 );
+        lobbyWindow.select(0);
         add(lobbyWindow);
 
         ExitButton btnExit = new ExitButton();
-        btnExit.setPos( Camera.main.width - btnExit.width(), 0 );
-        add( btnExit );
+        btnExit.setPos(Camera.main.width - btnExit.width(), 0);
+        add(btnExit);
     }
 
     @Override
-    protected void onBackPressed() { PixelDungeonCoop.switchNoFade( TitleScene.class ); }
+    protected void onBackPressed() {
+        PixelDungeonCoop.switchNoFade(TitleScene.class);
+    }
 }
